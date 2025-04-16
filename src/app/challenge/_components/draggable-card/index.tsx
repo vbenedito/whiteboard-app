f62bf2575a3acc41e41458885b386ca5754dfdd3
Vinicius Benedito <beneditoloura@gmail.com>
2025-04-16 19:01:31 -0300

    refactor: change component types

--- src/app/challenge/_components/draggable-card/index.tsx
+++ src/app/challenge/_components/draggable-card/index.tsx
@@ -1,9 +1,8 @@
 import { DraggableCardProps } from "./types";
 
-const DraggableCard = ({ type, label, onDragStart }: DraggableCardProps) => {
+const DraggableCard = ({ label, onDragStart }: DraggableCardProps) => {
   return (
     <div
-      key={type}
       draggable
       onDragStart={(e) => onDragStart(e, label)}
       className="p-2 bg-gray-700 rounded hover:bg-gray-600 cursor-grab"
